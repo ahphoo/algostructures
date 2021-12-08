c199d749a98eee05a406a106f8ef2825339b0e5b
aphoo <ahphu@ucsd.edu>
2021-12-07 22:51:51 -0800

    Added nav links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algostructures';
+  links = [
+    {url:"https://ahphoo.github.io/Advent of Code", name: "Advent of Code"},
+    {url:"https://ahphoo.github.io/Algoexpert", name: "Algoexpert"},
+    {url:"https://ahphoo.github.io/Cracking%20the%20Coding%20Interview/", name: "Cracking the Coding Interview"},
+    {url:"https://ahphoo.github.io/EPI/", name: "Elements of Programming Interviews"},
+    {url:"https://ahphoo.github.io/Javascript.Info/", name: "Javascript.Info"},
+    {url:"https://ahphoo.github.io/Linux/", name: "Linux"},
+    {url:"https://ahphoo.github.io/Resources/", name: "Resources"},
+    {url:"https://ahphoo.github.io/Yang%20Shun%20Handbook/", name: "Yang Shun Handbook"}
+  ];
 }
